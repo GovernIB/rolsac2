20d2391ded7e90432cb118db906cbad158f1dc41
slromero-indra <slromero-indra@minsait.com>
2023-10-25 14:09:12 +0200

    Había un fallo en el converter de normativas que se veía en las evoluciones

--- rolsac2-back/src/main/java/es/caib/rolsac2/back/converters/NormativaDTOConverter.java
+++ rolsac2-back/src/main/java/es/caib/rolsac2/back/converters/NormativaDTOConverter.java
@@ -1,5 +1,9 @@
 package es.caib.rolsac2.back.converters;
 
+import es.caib.rolsac2.service.facade.NormativaServiceFacade;
+import es.caib.rolsac2.service.model.NormativaDTO;
+import es.caib.rolsac2.service.model.NormativaGridDTO;
+
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
@@ -8,42 +12,41 @@ import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.inject.Named;
 
-import es.caib.rolsac2.service.facade.NormativaServiceFacade;
-import es.caib.rolsac2.service.model.NormativaDTO;
-
 @Named
 public class NormativaDTOConverter implements Converter {
 
-	@EJB
-	NormativaServiceFacade normativaService;
+    @EJB
+    NormativaServiceFacade normativaService;
 
-	@Override
-	public Object getAsObject(final FacesContext context, final UIComponent component, final String value) {
-		if (value == null || value.length() == 0) {
-			return null;
-		}
+    @Override
+    public Object getAsObject(final FacesContext context, final UIComponent component, final String value) {
+        if (value == null || value.length() == 0) {
+            return null;
+        }
 
-		final Long id;
-		try {
-			id = Long.parseLong(value);
-		} catch (final NumberFormatException nfe) {
-			final String error = "normativa_no_valido";
-//			final FacesMessage facesMessage = MessageFactory.getMessage(error, FacesMessage.SEVERITY_ERROR);
-			FacesMessage msg = new FacesMessage("Error", error);
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-			throw new ConverterException(msg);
-		}
+        final Long id;
+        try {
+            id = Long.parseLong(value);
+        } catch (final NumberFormatException nfe) {
+            final String error = "normativa_no_valido";
+            //			final FacesMessage facesMessage = MessageFactory.getMessage(error, FacesMessage.SEVERITY_ERROR);
+            FacesMessage msg = new FacesMessage("Error", error);
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            throw new ConverterException(msg);
+        }
 
-		return normativaService.findById(id);
-	}
+        return normativaService.findById(id);
+    }
 
-	@Override
-	public String getAsString(final FacesContext context, final UIComponent component, final Object value) {
-		if (value != null && value instanceof NormativaDTO && ((NormativaDTO) value).getCodigo() != null) {
-			return ((NormativaDTO) value).getCodigo().toString();
-		} else {
-			return "";
-		}
-	}
+    @Override
+    public String getAsString(final FacesContext context, final UIComponent component, final Object value) {
+        if (value != null && value instanceof NormativaDTO && ((NormativaDTO) value).getCodigo() != null) {
+            return ((NormativaDTO) value).getCodigo().toString();
+        } else if (value != null && value instanceof NormativaGridDTO && ((NormativaGridDTO) value).getCodigo() != null) {
+            return ((NormativaGridDTO) value).getCodigo().toString();
+        } else {
+            return "";
+        }
+    }
 
 }
