e9eddff03a362686c836ee60134bcd2c2a6fe562
slromero-indra <slromero-indra@minsait.com>
2025-03-12 13:15:27 +0100

    Fix #164
    - Quitado el parámetro java

--- rolsac2-service/src/main/java/es/caib/rolsac2/service/model/types/TypePropiedadConfiguracion.java
+++ rolsac2-service/src/main/java/es/caib/rolsac2/service/model/types/TypePropiedadConfiguracion.java
@@ -25,10 +25,6 @@ public enum TypePropiedadConfiguracion {
      */
     PROCESOS_MIN_MAX_MAESTRO_ACTIVO("procesos.minMaxMaestroInactivo"),
     /**
-     * LOPD
-     **/
-    LOPD_FINALIDAD("es.caib.rolsac2.lopd.finalidad"), LOPD_DESTINATARIO("es.caib.rolsac2.lopd.destinatario"), LOPD_DERECHOS("es.caib.rolsac2.lopd.derechos"), LOPD_RESPONSABLE("es.caib.rolsac2.lopd.responsable"), LOPD_CABECERA("es.caib.rolsac2.lopd.cabecera"), LOPD_PLANTILLA("es.caib.rolsac2.lopd.plantilla"),
-    /**
      * UA COMUN
      **/
     UA_COMUN("es.caib.rolsac2.comun.ua"),
