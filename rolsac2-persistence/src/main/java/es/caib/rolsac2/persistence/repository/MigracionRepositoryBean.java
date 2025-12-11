e7db355e887f51fcadb087f4ba91d5b4ab8c6489
slromero-indra <slromero-indra@minsait.com>
2025-12-11 18:05:35 +0100

    Fix #267

--- rolsac2-persistence/src/main/java/es/caib/rolsac2/persistence/repository/MigracionRepositoryBean.java
+++ rolsac2-persistence/src/main/java/es/caib/rolsac2/persistence/repository/MigracionRepositoryBean.java
@@ -60,10 +60,27 @@ public class MigracionRepositoryBean extends AbstractCrudRepository<JProceso, Lo
         query.execute();
     }
 
+    /**
+     * Se tiene que devolver la lista de ambas query quitando repetidos.
+     *
+     * @param idEntidad
+     * @return
+     */
     @Override
     public List<BigDecimal> getNormativas(Long idEntidad) {
-        Query query = this.entityManager.createNativeQuery("   SELECT NOR_CODI  FROM R1_NORMAT WHERE NOR_CODUNA IN (SELECT UNAD_CODIGO FROM RS2_UNIADM)");
-        return query.getResultList();
+        Query query = this.entityManager.createNativeQuery("   SELECT NOR_CODI  FROM R1_NORMAT WHERE NOR_CODUNA IN (SELECT UNAD_CODIGO FROM RS2_UNIADM) " +
+                " UNION " +
+                " SELECT DISTINCT UNN_CODNOR FROM RSC_UNANOR WHERE UNN_CODUNA IN (SELECT UNAD_CODIGO FROM RS2_UNIADM) ");
+
+        List<BigDecimal> retornoSQL = query.getResultList();
+        //Quitar duplicados
+        List<BigDecimal> retorno = new ArrayList<>();
+        for (BigDecimal bigDecimal : retornoSQL) {
+            if (!retorno.contains(bigDecimal)) {
+                retorno.add(bigDecimal);
+            }
+        }
+        return retorno;
     }
 
     @Override
