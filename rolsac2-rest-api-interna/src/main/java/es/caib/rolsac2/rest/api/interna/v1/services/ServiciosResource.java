15d60256590c941b0d28c0ab392d402e244559e6
slromero-indra <slromero-indra@minsait.com>
2026-03-25 18:33:24 +0100

    Fix #288

--- rolsac2-rest-api-interna/src/main/java/es/caib/rolsac2/rest/api/interna/v1/services/ServiciosResource.java
+++ rolsac2-rest-api-interna/src/main/java/es/caib/rolsac2/rest/api/interna/v1/services/ServiciosResource.java
@@ -124,7 +124,7 @@ public class ServiciosResource {
 
         // si no vienen los filtros se completan con los datos por defecto
         if (filtro.getFiltroPaginacion() != null) {
-            fg.setPaginaTamanyo(Integer.parseInt(systemService.obtenerPropiedadConfiguracion(TypePropiedadConfiguracion.API_MAX_LIMIT)));
+            fg.setPaginaTamanyo(filtro.getFiltroPaginacion().getSize());
             fg.setPaginaFirst(filtro.getFiltroPaginacion().getOffset());
         }
 
